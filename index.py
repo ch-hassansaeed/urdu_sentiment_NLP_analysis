e58d6a9038270384731701a53ef56be8d9e93f4c
ch-hassansaeed <ch.hassansaeed@gmail.com>
2018-05-26 16:22:37 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,6 @@
+﻿from urdu_sentiment_NLP_analysis_lib import get_urdu_sentiment
+urdu_input_sentence="""
+آپ جیسا اچھا بندہ نہیں دیکھا میں نے   
+"""
+urdu_sentiment_output=get_urdu_sentiment(urdu_input_sentence)
+print(urdu_sentiment_output)
